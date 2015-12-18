35ab943a6c22a7b61907e0a45a79a053829543d4
suzheng <294839757@qq.com>
2015-12-18 16:20:18 +0800

    用户修改

--- application/models/user/User_model.php
+++ application/models/user/User_model.php
@@ -97,10 +97,11 @@ class User_model extends MY_Model {
     
     function getUserIdsByFiled($data){
         $data['token']=urlencode($data['token']);
+        echo $data['token'];
         $url=$this->user_api_url."/user/getUserIdsByFiled";
       //  echo $url;
         $return=$this->curl($url,$data);
-     //   var_dump($return);
+       var_dump($return);
         return $return;
     }
     
