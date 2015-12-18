20467f67e98810f0d89e9c5fdd001e8f914a6e4c
suzheng <294839757@qq.com>
2015-12-18 16:01:05 +0800

    修图审核修改

--- application/models/retouch/Retouch_model.php
+++ application/models/retouch/Retouch_model.php
@@ -10,6 +10,7 @@ class Retouch_model extends MY_Model
     }
     /*获取修图列表*/
      function getMarlboroList($data){
+         var_dump($data);
          $url=$this->more_api_url."/lingmall/audit/list";
          echo $url;
          $return=$this->curl($url,$data,'get');
