561cdbddea4c9f4b6b0f0dc33e4fff0c9d9f9094
suzheng <294839757@qq.com>
2015-12-25 11:11:38 +0800

    拍摄反馈的修改

--- application/models/ps/Marlboro_model.php
+++ application/models/ps/Marlboro_model.php
@@ -268,11 +268,9 @@ class Marlboro_model extends MY_Model {
     /*获取拍摄反馈的数据*/
     function getShootBackManager($data){
         $data['token']=urldecode($data['token']);
+        var_dump($data['token']);
         $url=$this->more_api_url."/lingmall/feed/list";
         $return=$this->curl($url,$data,'get');
-        if(!$return){
-            $return=file_get_contents("f:/wamp/www/curl/marlboro/feedBack.txt");
-        }
         $datas=json_decode($return,true);
         $back=$this->back($datas,$data);
         return $back;
